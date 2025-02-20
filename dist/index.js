49a3521007437e813efe8dd9eaa1b907f0731528
Wilfried <wil.dpt2015@gmail.com>
2025-02-20 14:34:16 +0100

    correction: Suppresion est modification de fichier js

---
+++ dist/index.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const PostService_1 = __importDefault(require("./services/PostService"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+app.use(express_1.default.urlencoded({ extended: true }));
+const postService = new PostService_1.default();
+app.use('/', (req, res) => {
+    res.render('home');
+});
+app.get('/posts', (req, res) => {
+    const posts = postService.getAllPosts();
+    res.render('posts', { posts });
+});
+app.post('/posts', (req, res) => {
+    const post = postService.createPost(req.body);
+    console.log(post);
+    res.redirect(`/posts`);
+});
+app.get('/posts/new', (req, res) => {
+    res.render('new-post');
+});
+app.get('/posts/:id/edit', (req, res) => {
+    const post = postService.getPostById(parseInt(req.params.id));
+    if (!post) {
+        res.status(404).json({ error: 'Post not found' });
+        return;
+    }
+    res.render('edit-post', { post });
+});
+app.get('/posts/:id', (req, res) => {
+    const post = postService.getPostById(parseInt(req.params.id));
+    if (!post) {
+        res.status(404).json({ error: 'Post not found' });
+        return;
+    }
+    res.render('post', { post });
+});
+app.post('/posts/:id', (req, res) => {
+    console.log(req.params.id);
+    const updatedPost = postService.updatePost(parseInt(req.params.id), req.body);
+    res.redirect(`/posts/${req.params.id}`);
+});
---
+++ dist/services/PostService.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const data_json_1 = __importDefault(require("../data.json"));
+class PostService {
+    constructor() {
+        this.posts = [...data_json_1.default.posts];
+    }
+    getAllPosts() {
+        return this.posts;
+    }
+    getPostById(id) {
+        return this.posts.find((post) => post.id === id);
+    }
+    createPost(postData) {
+        const newPost = {
+            id: this.posts.length + 1,
+            title: postData.title,
+            content: postData.content,
+            author: postData.author,
+            createdAt: new Date().toISOString(),
+        };
+        this.posts.push(newPost);
+        return newPost;
+    }
+    updatePost(id, postData) {
+        const index = this.posts.findIndex((post) => post.id === id);
+        if (index === -1)
+            return null;
+        this.posts[index] = Object.assign(Object.assign({}, this.posts[index]), { title: postData.title, content: postData.content, author: postData.author, updatedAt: new Date().toISOString() });
+        return this.posts[index];
+    }
+}
+exports.default = PostService;
