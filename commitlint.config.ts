e7b07d25bf767326c077830ac34d406424c77c4f
Wilfried <wil.dpt2015@gmail.com>
2025-02-27 11:50:03 +0100

    ajout: Ajout d'un parametre de commit

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,7 +3,7 @@ import type { UserConfig } from "@commitlint/types";
 const Configuration: UserConfig = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "type-enum": [2, "always", ["fonctionnalité", "correction de bug", "correction", "ajout", "test", "config"]],
+        "type-enum": [2, "always", ["fonctionnalité", "correction de bug", "correction", "ajout", "test", "config", "mise à jour"]],
         "subject-case": [2, "always", "sentence-case"], // Le message commence par une majuscule
         "header-max-length": [2, "always", 100] // Limite à 100 caractères max
     }
